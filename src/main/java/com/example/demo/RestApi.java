9911bfbd298ea171e21ed730d60243b724214d59
avi5kdon <avi5kdon@gmail.com>
2021-06-22 22:07:12 +0530

    text

---
+++ src/main/java/com/example/demo/RestApi.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Properties;
+
+@RestController
+public class RestApi {
+    private static byte[] bytes;
+private static KafkaProducer<String, String> producer;
+static {
+    Properties configProps = new Properties();
+
+    configProps.put("security.protocol","ssl");
+    configProps.put("ssl.truststore.location",RestApi.class.getClassLoader().getResource("truststore.jks").getFile());
+    configProps.put("ssl.truststore.password","password");
+    configProps.put("ssl.truststore.type","JKS");
+    configProps.put(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+            "my-cluster-kafka-bootstrap.default:9093");
+    configProps.put(
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class);
+    configProps.put(
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+            StringSerializer.class);
+
+    producer = new KafkaProducer<String, String>(configProps);
+
+}
+    @GetMapping("/test")
+    public String test(@RequestParam String param){
+
+
+         ProducerRecord<String, String > producerRecord = new ProducerRecord<>("my-topic",param);
+         producer.send(producerRecord);
+         System.out.println(">> Sending something >>");
+
+        return "Success";
+    }
+}
