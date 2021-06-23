4c1cbefc7855459181f65d953cbd0e834f16a246
Avinash Dongre <avi5kdon@gmail.com>
2021-06-23 11:38:10 +0530

    gfe

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -4,14 +4,12 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.misc.ClassLoaderUtil;
 
-import java.io.*;
+import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 @RestController
@@ -25,7 +23,7 @@ static {
         InputStream fis = RestApi.class.getClassLoader().getResourceAsStream("truststore.jks");
         byte[] bytes = new byte[fis.available()];
         fis.read(bytes);
-        FileOutputStream fos = new FileOutputStream("/home/passwd/truststore.jks");
+        FileOutputStream fos = new FileOutputStream("/home/jboss/truststore.jks");
         fos.write(bytes);
         fos.flush();
         fos.close();
@@ -37,7 +35,7 @@ static {
     }
     Properties configProps = new Properties();
     configProps.put("security.protocol","ssl");
-    configProps.put("ssl.truststore.location", "/home/passwd/truststore.jks");
+    configProps.put("ssl.truststore.location", "/home/jboss/truststore.jks");
     configProps.put("ssl.truststore.password","password");
     configProps.put("ssl.truststore.type","JKS");
     configProps.put(
