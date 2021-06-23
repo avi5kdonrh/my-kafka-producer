21598725e5a7effec6d4a69f5648dcf057347155
Avinash Dongre <avi5kdon@gmail.com>
2021-06-23 13:10:05 +0530

    new-sources

--- src/main/java/com/example/demo/RestApi.java
+++ src/main/java/com/example/demo/RestApi.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,53 +13,24 @@ import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.Properties;
 
-//@RestController
+@Configuration
 public class RestApi {
 
-
-    private static byte[] bytes;
-private static KafkaProducer<String, String> producer;
-static {
-    try {
-        InputStream fis = RestApi.class.getClassLoader().getResourceAsStream("truststore.jks");
-        byte[] bytes = new byte[fis.available()];
-        fis.read(bytes);
-        FileOutputStream fos = new FileOutputStream("/home/jboss/truststore.jks");
-        fos.write(bytes);
-        fos.flush();
-        fos.close();
-        fis.close();
+    static {
+        try {
+            InputStream fis = RestApi.class.getClassLoader().getResourceAsStream("truststore.jks");
+            byte[] bytes = new byte[fis.available()];
+            fis.read(bytes);
+            FileOutputStream fos = new FileOutputStream("/home/jboss/truststore.jks");
+            fos.write(bytes);
+            fos.flush();
+            fos.close();
+            fis.close();
 
 
-    }catch (Exception ex){
-        ex.printStackTrace();
-    }
-    Properties configProps = new Properties();
-    configProps.put("security.protocol","ssl");
-    configProps.put("ssl.truststore.location", "/home/jboss/truststore.jks");
-    configProps.put("ssl.truststore.password","password");
-    configProps.put("ssl.truststore.type","JKS");
-    configProps.put(
-            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-            "my-cluster-kafka-bootstrap.amqs:9093");
-    configProps.put(
-            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class);
-    configProps.put(
-            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-            StringSerializer.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
-    producer = new KafkaProducer<String, String>(configProps);
-
-}
-    @GetMapping("/test")
-    public String test(@RequestParam String param){
-
-
-         ProducerRecord<String, String > producerRecord = new ProducerRecord<>("my-topic",param);
-         producer.send(producerRecord);
-         System.out.println(">> Sending something >>");
-
-        return "Success";
     }
 }
